8bb134bcb4ec34560c644b184d0d51320fd7e482
24DP1TRusi <A240167TR@rvt.lv>
2025-09-18 12:29:42 +0300

    Grades and points

---
+++ src/main/java/rvt/GradesAndPoints.java
@@ -0,0 +1,31 @@
+package rvt;
+
+import java.util.Scanner;
+
+public class GradesAndPoints {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Give points [0-100]:");
+        int number = Integer.valueOf(scanner.nextLine());
+
+        if (number < 0) {
+            System.out.println("Grade: impossible!");
+        } else if (number <= 49) {
+            System.out.println("Grade: failed");
+        } else if (number <= 59) {
+            System.out.println("Grade: 1");
+        } else if (number <= 69) {
+            System.out.println("Grade: 2");
+        } else if (number <= 79) {
+            System.out.println("Grade: 3");
+        } else if (number <= 89) {
+            System.out.println("Grade: 4");
+        } else if (number <= 100) {
+            System.out.println("Grade: 5");
+        } else if (number > 100) {
+            System.out.println("Grade: incredible!");
+        }
+        scanner.close();
+    }
+
+}
